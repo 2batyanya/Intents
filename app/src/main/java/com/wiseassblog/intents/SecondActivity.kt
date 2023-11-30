f4bf2bb7ee7f48a6033ee1005fae0cf70b968781
Ilya <2batyanya@gmail.com>
2023-11-30 18:55:36 +0300

    Initial commit

---
+++ app/src/main/java/com/wiseassblog/intents/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.wiseassblog.intents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
